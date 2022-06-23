be607cc9beaf2868410d832bb3582dbd4b0dda37
Baha Taşkın <77745716+MischieS@users.noreply.github.com>
2022-06-23 03:40:15 +0300

    Add files via upload

---
+++ libft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: btaskin <btaskin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/23 01:10:21 by btaskin           #+#    #+#             */
+/*   Updated: 2022/06/23 03:33:57 by btaskin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdio.h>
+
+int	ft_isalpha(int i);
+int	ft_isdigit(int i);
+int	ft_isalnum(int i);
+int	ft_isascii(int i);
+
+#endif
